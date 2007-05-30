a7178213d472c7863af9cf8fbbaa4a9f23120e62
cmsbuild <>
2007-05-30 09:17:14 +0000

    This commit was manufactured by cvs2git to create branch 'DataFormats- Candidate-AnalysisTools_1_3_5_cand0'.
    Sprout from DataFormats-Candidate-B1_3_X_AnalysisTools 2007-05-11 10:57:14 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'patch for 13X'
    Cherrypick from master 2007-05-30 09:17:05 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added missing implementations and missing dictionaries':
        src/classes.h
        src/classes_def.xml

--- src/classes.h
+++ src/classes.h
@@ -20,24 +20,25 @@ namespace {
     reco::CandidateRef r1;
     reco::CandidateBaseRef r2;
     reco::CandidateRefVector rv1;
+    edm::reftobase::IndirectHolder<reco::Candidate> rbih1;
+    edm::reftobase::RefHolder<reco::CandidateRef> rh1;
     edm::Wrapper<reco::CandidateRefVector> wrv1;
     reco::CandidateRefProd rp1;
     std::vector<edm::RefToBase<reco::Candidate> > vrb1;
     edm::reftobase::Holder<reco::Candidate, reco::CandidateRef> rhcr1;
-    edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<double> > av1;
     edm::Wrapper<edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<double> > > wav1;
-    edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<float> > av2;
     edm::Wrapper<edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<float> > > wav2;
-    edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<int> > av3;
     edm::Wrapper<edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<int> > > wav3;
+    edm::Wrapper<edm::AssociationVector<edm::RefProd<reco::CandidateCollection>, std::vector<unsigned int> > > wav4;
     edm::helpers::KeyVal<reco::CandidateRef,reco::CandidateRef> kv1;
     reco::CandMatchMap cmm1;
     reco::CandMatchMap::const_iterator cmm1it;
     edm::Wrapper<reco::CandMatchMap> wcmm1;
-    edm::helpers::KeyVal<edm::RefProd<edm::OwnVector<reco::Candidate,edm::ClonePolicy<reco::Candidate> > > ,edm::RefProd<edm::OwnVector<reco::Candidate,edm::ClonePolicy<reco::Candidate> > > > kv2;
+    edm::helpers::KeyVal<reco::CandidateRefProd, reco::CandidateRefProd> kv2;
     std::map<const reco::Candidate *, const reco::Candidate *> m1;
     std::vector<const reco::Candidate *> vc1;
     reco::CandMatchMapMany cmm2;
     edm::Wrapper<reco::CandMatchMapMany> wcmm2;
+    edm::Wrapper<std::vector<reco::CandidateBaseRef> > wvrb1;
   }
 }
